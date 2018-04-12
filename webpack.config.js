aa410ed61ff8bf5be72e032f25677425a0ee9225
RenzoC <renzocallachavez@gmail.com>
2018-04-11 21:01:51 -0400

    setup devDependencies

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = env => {
   return {
     entry:'./src/app.js',
     output: {
-      path: path.resolve(__dirname,'public'),
+      path: path.resolve(__dirname,'public','dist'),
       filename: 'bundle.js'
     },
     module:{
@@ -47,6 +47,7 @@ module.exports = env => {
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.resolve(__dirname,'public'),
+      publicPath:'/dist/',
       port: 9000,
       historyApiFallback:true
     }
